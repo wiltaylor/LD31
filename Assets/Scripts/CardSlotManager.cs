73d5c9f5c8261bffefc5bb902644eb500cbe092a
Wil Taylor <wilfridtaylor@gmail.com>
2014-12-06 23:12:47 +1100

    Have most of card handler code done. Some corruption a few times with scene causing me to recreate it all.

--- Assets/Scripts/CardSlotManager.cs
+++ Assets/Scripts/CardSlotManager.cs
@@ -1,7 +1,9 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using UnityEngine;
 using System.Collections;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class CardSlotManager : MonoBehaviour
 {
@@ -11,21 +13,53 @@ public class CardSlotManager : MonoBehaviour
     public float SpaceBetwenCards = 1f;
     public float CardWidth = 60f;
     public int Owner = 0;
+    public int CardsBeforeScroll = 8;
+
+    private Slider _slider;
+    private int _currentRow = 0;
 
     public void Start()
     {
+        for (var i = 0; i < transform.childCount; i++)
+        {
+            var child = transform.GetChild(i);
+            if (child.name == "Slider")
+                _slider = child.gameObject.GetComponent<Slider>();
+        }
+
+        SortCards();
+    }
+
+    public void SetRow()
+    {
+        _currentRow  = (int)_slider.value;
         SortCards();
     }
 
     public void SortCards()
     {
-        var cards = from c in GetComponentsInChildren<CardManager>()
-                    select c.gameObject;
+        var cards = (from c in GetComponentsInChildren<CardManager>()
+                    select c.gameObject).ToArray();
 
         var index = 0;
+        var waitcount = _currentRow*CardsBeforeScroll;
 
         foreach (var c in cards)
         {
+            if (waitcount > 0)
+            {
+                waitcount--;
+                c.SetActive(false);
+                continue;
+            }
+
+            if (index >= CardsBeforeScroll)
+            {
+                c.SetActive(false);
+                continue;
+            }
+
+            c.SetActive(true);
             var rt = c.GetComponent<RectTransform>();
             rt.position = new Vector3(FirstCardX + ((CardWidth + SpaceBetwenCards) * index) + transform.position.x, FirstCardY + transform.position.y);
             index++;
